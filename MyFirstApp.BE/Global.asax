6374f1a6da5d79a4c44c4ee927c9bfdedb79b852
brodzinski <brodzinski@interia.pl>
2018-11-05 21:26:08 +0100

    Inicjalizacja projektu

---
+++ MyFirstApp.BE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyFirstApp.BE.WebApiApplication" Language="C#" %>
---
+++ MyFirstApp.BE/Models/LoginData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstApp.BE.Models
+{
+    public class LoginData
+    {
+    }
+}
